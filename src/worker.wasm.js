0c76b861b6e6ba70b0b67bffcd01a4eb250472ad
jvail <jvail@users.noreply.github.com>
2017-12-29 17:41:48 +0100

    fix worker

--- src/worker.wasm.js
+++ src/worker.wasm.js
@@ -1,73 +1,73 @@
-var ww_createDb, ww_db;
-
 if (typeof importScripts === 'function') {
-  ww_db = null, spatiasql = spatiasql();
-  ww_createDb = function(data) {
-    if (ww_db != null) {
-      ww_db.close();
-    }
-    return ww_db = new spatiasql.Database(data);
-  };
-  self.onmessage = function(event) {
-    var buff, callback, data, done, err;
-    data = event['data'];
-    switch (data != null ? data['action'] : void 0) {
-      case 'open':
-        buff = data['buffer'];
-        ww_createDb((buff ? new Uint8Array(buff) : void 0));
-        return postMessage({
-          'id': data['id'],
-          'ready': true
-        });
-      case 'exec':
-        if (ww_db === null) {
-          ww_createDb();
+  spatiasql(function (s) {
+    var db = null,
+      createDb = function(data) {
+        if (db != null) {
+          db.close();
         }
-        if (!data['sql']) {
-          throw new Error('exec: Missing query string');
-        }
-        return postMessage({
-          'id': data['id'],
-          'results': ww_db.exec(data['sql'])
-        });
-      case 'each':
-        if (ww_db === null) {
-          ww_createDb();
-        }
-        callback = function(row) {
+        return db = new s.Database(data);
+      };
+    self.onmessage = function(event) {
+      var buff, callback, data, done, err;
+      data = event['data'];
+      switch (data != null ? data['action'] : void 0) {
+        case 'open':
+          buff = data['buffer'];
+          createDb((buff ? new Uint8Array(buff) : void 0));
           return postMessage({
             'id': data['id'],
-            'row': row,
-            'finished': false
+            'ready': true
           });
-        };
-        done = function() {
+        case 'exec':
+          if (db === null) {
+            createDb();
+          }
+          if (!data['sql']) {
+            throw new Error('exec: Missing query string');
+          }
           return postMessage({
             'id': data['id'],
-            'finished': true
+            'results': db.exec(data['sql'])
           });
-        };
-        return ww_db.each(data['sql'], data['params'], callback, done);
-      case 'export':
-        buff = ww_db["export"]().buffer;
-        try {
-          return postMessage({
-            'id': data['id'],
-            'buffer': buff
-          }, [buff]);
-        } catch (_error) {
-          err = _error;
-          return postMessage({
-            'id': data['id'],
-            'buffer': buff
-          });
-        }
-        break;
-      case 'close':
-        return ww_db != null ? ww_db.close() : void 0;
-      default:
-        throw new Error('Invalid action : ' + (data != null ? data['action'] : void 0));
-    }
-  };
-  postMessage({ initialized: true });
+        case 'each':
+          if (db === null) {
+            createDb();
+          }
+          callback = function(row) {
+            return postMessage({
+              'id': data['id'],
+              'row': row,
+              'finished': false
+            });
+          };
+          done = function() {
+            return postMessage({
+              'id': data['id'],
+              'finished': true
+            });
+          };
+          return db.each(data['sql'], data['params'], callback, done);
+        case 'export':
+          buff = db["export"]().buffer;
+          try {
+            return postMessage({
+              'id': data['id'],
+              'buffer': buff
+            }, [buff]);
+          } catch (_error) {
+            err = _error;
+            return postMessage({
+              'id': data['id'],
+              'buffer': buff
+            });
+          }
+          break;
+        case 'close':
+          return db != null ? db.close() : void 0;
+        default:
+          throw new Error('Invalid action : ' + (data != null ? data['action'] : void 0));
+      }
+    };
+    postMessage({ initialized: true });
+  });
 }
